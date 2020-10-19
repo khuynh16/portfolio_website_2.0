47ad5cd6c25e4a5e72fc863189b69338d75fd054
Kelvin Huynh <kelvinahuynh@gmail.com>
2020-10-19 17:22:51 -0400

    added correct timer; web responsiveness of font size

--- src/app/body/receipt/receipt-heading/receipt-heading.component.ts
+++ src/app/body/receipt/receipt-heading/receipt-heading.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./receipt-heading.component.css']
 })
 export class ReceiptHeadingComponent implements OnInit {
+  currentTime: string;
+  currentHour: number;
+  currentMinute: string;
+  amOrPm: string = 'AM';
+  dateObj: Date;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.dateObj = new Date();
+
+    this.currentTime = this.getNonMilitaryTime(this.dateObj.getHours(), this.dateObj.getMinutes());
   }
 
+  getNonMilitaryTime(hours, minutes) {
+    if (hours > 12) {
+      this.currentHour = hours - 12;
+      this.amOrPm = 'PM';
+    } else {
+      this.currentHour = hours;
+    }
+
+    if (minutes < 10) {
+      this.currentMinute = '0' + minutes;
+    } else {
+      this.currentMinute = minutes;
+    }
+
+    return this.currentHour + ':' + this.currentMinute + ' ' + this.amOrPm;
+  }
 }
